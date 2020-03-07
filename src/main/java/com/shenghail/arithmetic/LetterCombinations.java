f1f72efd3afd34988834b671f9fca631a0eddd43
ShinHei-Lin <1904971860@qq.com>
2020-03-07 17:18:24 +0800

    Changes to be committed: 	new file:   .idea/compiler.xml 	new file:   .idea/encodings.xml 	new file:   .idea/libraries/Maven__junit_junit_4_13.xml 	new file:   .idea/libraries/Maven__org_hamcrest_hamcrest_core_1_3.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/workspace.xml 	new file:   arithmeticDemo.iml 	new file:   pom.xml 	new file:   src/main/java/com/shenghail/arithmetic/LetterCombinations.java 	new file:   src/test/java/com/shenghail/arithmetic/LetterCombinationsTest.java 	new file:   target/classes/com/shenghail/arithmetic/LetterCombinations.class 	new file:   target/test-classes/com/shenghail/arithmetic/LetterCombinationsTest.class

---
+++ src/main/java/com/shenghail/arithmetic/LetterCombinations.java
@@ -0,0 +1,111 @@
+package com.shenghail.arithmetic;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LetterCombinations {
+
+    /**
+     * 查找字母组合
+     * @param digits
+     * @return
+     */
+    public String letterCombinations(int[] digits) {
+        boolean flag = checkPram(digits);
+        if (!flag) {
+            return "";
+        }
+        digits = handleParam(digits);
+        List<String> result = new LinkedList<>();
+        String[] letterArr = new String[digits.length];
+        for (int i = 0; i < digits.length; i++) {
+            switch (digits[i]) {
+                case 2 : letterArr[i] = "abc"; break;
+                case 3 : letterArr[i] = "def"; break;
+                case 4 : letterArr[i] = "ghi"; break;
+                case 5 : letterArr[i] = "jkl"; break;
+                case 6 : letterArr[i] = "mno"; break;
+                case 7 : letterArr[i] = "pqrs"; break;
+                case 8 : letterArr[i] = "tuv"; break;
+                case 9 : letterArr[i] = "wxyz"; break;
+                default: break;
+            }
+        }
+        permute(result, letterArr, 0, "");
+
+        String resultStr = listToString(result);
+
+        return resultStr;
+    }
+
+    /**
+     * 处理输入参数
+     * @param param
+     * @return
+     */
+    private int[] handleParam(int[] param) {
+        int count = 0;
+        for (int i = 0; i < param.length; i++) {
+            if (param[i] == 0 || param[i] == 1) {
+                ++count;
+                for (int j = i; j < param.length-1; j++) {
+                    param[j] = param[j+1];
+                }
+                i--;
+                System.out.println(Arrays.toString(param));
+            }
+        }
+        int[] result = Arrays.copyOfRange(param, 0, param.length-count);
+        return result;
+    }
+
+    /**
+     * 递归查找字母组合
+     * @param result
+     * @param letterArr
+     * @param index
+     * @param temp
+     */
+    private void permute(List<String> result, String[] letterArr, int index, String temp) {
+        if (index >= letterArr.length) {
+            result.add(temp);
+            return;
+        }
+        String indexStr = letterArr[index];
+        for (int i=0; i<indexStr.length(); i++) {
+            permute(result, letterArr , index+1, temp+indexStr.charAt(i));
+        }
+    }
+
+    /**
+     * 检查输入参数合法性
+     * @param param
+     * @return
+     */
+    private boolean checkPram(int[] param) {
+        if (param == null || param.length <= 0) {
+            return false;
+        }
+        for (int i = 0; i < param.length; i++) {
+            if (param[i] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 将集合转换成字符串类型
+     * @param obj
+     * @return
+     */
+    private String listToString(List<String> obj){
+        StringBuilder resultStr = new StringBuilder();
+        for (String s : obj) {
+            resultStr.append(s);
+            resultStr.append(" ");
+        }
+        return resultStr.toString().trim();
+    }
+}
---
+++ src/test/java/com/shenghail/arithmetic/LetterCombinationsTest.java
@@ -0,0 +1,63 @@
+package com.shenghail.arithmetic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LetterCombinationsTest {
+
+    @Test
+    public void letterCombinationsTest1() {
+        int[] input = {2,3};
+        String output = "ad ae af bd be bf cd ce cf";
+        LetterCombinations letterCombinations = new LetterCombinations();
+        String result = letterCombinations.letterCombinations(input);
+        Assert.assertEquals(result, output);
+    }
+
+    @Test
+    public void letterCombinationsTest2() {
+        int[] input = {9};
+        String output = "w x y z";
+        LetterCombinations letterCombinations = new LetterCombinations();
+        String result = letterCombinations.letterCombinations(input);
+        Assert.assertEquals(result, output);
+    }
+
+    @Test
+    public void letterCombinationsTest3() {
+        int[] input = {};
+        String output = "";
+        LetterCombinations letterCombinations = new LetterCombinations();
+        String result = letterCombinations.letterCombinations(input);
+        Assert.assertEquals(result, output);
+    }
+
+    @Test
+    public void letterCombinationsTest4() {
+        int[] input = {-1};
+        String output = "";
+        LetterCombinations letterCombinations = new LetterCombinations();
+        String result = letterCombinations.letterCombinations(input);
+        Assert.assertEquals(result, output);
+    }
+
+    @Test
+    public void letterCombinationsTest5() {
+        int[] input = {1,1,2,3,0,0,4};
+        String output = "adg adh adi aeg aeh aei afg afh afi bdg bdh bdi beg beh " +
+                        "bei bfg bfh bfi cdg cdh cdi ceg ceh cei cfg cfh cfi";
+        LetterCombinations letterCombinations = new LetterCombinations();
+        String result = letterCombinations.letterCombinations(input);
+        System.out.println(result);
+        Assert.assertEquals(result, output);
+    }
+
+    @Test
+    public void letterCombinationsTest6() {
+        int[] input = {0,1,2,3,4,5,6,7,8,9};
+        String output = "";
+        LetterCombinations letterCombinations = new LetterCombinations();
+        String result = letterCombinations.letterCombinations(input);
+        Assert.assertNotNull(result);
+    }
+}
